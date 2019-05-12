7818a7b28213d1f0eb5822651fca51080af9d17f
Mustafa Tekeli <mustafatekeli.mt@gmail.com>
2019-05-12 22:09:30 +0300

    init members in constructor

--- src/app.cpp
+++ src/app.cpp
@@ -23,11 +23,19 @@ App::App(int argc, char* argv[]) : QGuiApplication(argc, argv)
     // "Measurement*", "Cannot init from QML");
     qRegisterMetaType<Measurement*>("Measurement");
 
+    _settings = new AppSettings{QGuiApplication::organizationName(),
+                                QGuiApplication::applicationName(), this};
+    _rs = new RoomService{_settings->roomServiceUrl(),
+                          _settings->roomServiceInterval(), this};
+    _ls = new LocationService{
+        QStringLiteral("https://api.ipdata.co/?api-key=%1").arg(IPDATA_API_KEY),
+        this};
+
     engine.addImportPath("qrc:/");
     engine.rootContext()->setContextProperty(QStringLiteral("RoomService"),
-                                             &_rs);
+                                             _rs);
     engine.rootContext()->setContextProperty(QStringLiteral("LocationService"),
-                                             &_ls);
+                                             _ls);
     engine.rootContext()->setContextProperty(QStringLiteral("App"), this);
     engine.load(QUrl{QStringLiteral("qrc:/MainWindow.qml")});
 }
