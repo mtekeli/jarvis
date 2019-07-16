cf9b91445d253ba9a1495bb8204bbc68d04cb8db
Mustafa Tekeli <mustafatekeli.mt@gmail.com>
2019-07-16 19:25:49 +0300

    exit if qt5 dir is not set

--- sh/build-macos.sh
+++ sh/build-macos.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-export Qt5_DIR="/Users/mustafatekeli/Qt/5.12.3/clang_64"
+if [[ -z "${Qt5_DIR}" ]]; then
+  echo "set Qt5_DIR environment variable first and re-run this. e.g. export Qt5_DIR=~/Qt/5.12.4/clang_64"
+  exit 1
+fi
+
 export BUILD_NAME="build-amd64-darwin"
 
 make $1
